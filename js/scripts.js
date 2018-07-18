fddf87c382567ca4b35700c6e26184283419fe1c
IreriVikki <wambsviki@gmail.com>
2018-07-18 17:31:04 +0300

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+/**
+ rando generator Math.random()
+    parseInt(Math.random() * 10)
+
+1. player rolls a dice
+2. if number of dice is 1 player stops rolling and scores nothing
+    > else player rolls again summing the totoal of his numbers and plays again
+    > if player chooses to hold, they stop playing and their numbers are summed up
+
+3. first player to score 100 point wins the game
+ */
+
+ /*
+ #####################################################################
+ > procedure for the project
+ 1. build ui with the following features
+    a. players enter their names
+    b. players choose their avaters
+    c. empty arrays are created for each player to hold their rolls
+    d. empty total variable to hold each of the players total sum of numbers
+    e. random number generating function is created
+    f. function to check if number is 1 or not
+    g. push number to enpty array or empty the array if number is 1
+    h. add numbers in array
+    i. add sum of array nimbers to sum of player
+    j. 
+    j. switch to player 2 and repeat the process
+    
+2. button for rolling the dice
+3. seperate result fields one for ongoing count and another for completed total
+4. 
+ */
+
+ console.log(parseInt(Math.random()*10))
