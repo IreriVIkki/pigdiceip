f3794d1933a9562e1a5e092f1986cd6a64ff5847
IreriVikki <wambsviki@gmail.com>
2018-07-18 18:12:15 +0300

    created function for rolling dice

--- js/scripts.js
+++ js/scripts.js
@@ -20,15 +20,32 @@
     d. empty total variable to hold each of the players total sum of numbers
     e. random number generating function is created
     f. function to check if number is 1 or not
+        > check if both dices are 1 and player loses all points
     g. push number to enpty array or empty the array if number is 1
     h. add numbers in array
     i. add sum of array nimbers to sum of player
-    j. 
-    j. switch to player 2 and repeat the process
+    j. check if  player total is 100 and declare then a winner
+    k. declare the oposition a loser by default otherwise.....
+    l. switch to player 2 and repeat the process
     
 2. button for rolling the dice
 3. seperate result fields one for ongoing count and another for completed total
 4. 
  */
 
- console.log(parseInt(Math.random()*10))
+function rollDice(){
+    for(;;){
+        var number = parseInt(Math.random() * 10)
+        if (number > 0 && number < 7){
+            console.log(number)
+            return number
+            break
+        }
+    }
+}
+rollDice()
+var dice1 = rollDice()
+var dice2 = rollDice()
+function playerRolls(numb1, numb2){
+    
+}
